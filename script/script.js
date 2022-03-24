6e4d7c2293ffe2e45c95f600bb81128d50254be0
LLEGIT <theogillet.bordeaux@outlook.com>
2022-03-24 09:35:31 +0100

    ajout de style

--- script/script.js
+++ script/script.js
@@ -1,3 +1,3 @@
 document.addEventListener("DOMContentLoaded", function() {
-    alert("Hello there ! Hope you enjoy the visit of my resume")
+    // alert("Hello there ! Hope you enjoy the visit of my resume")
 })
